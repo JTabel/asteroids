dfd6ec0a84bb1ca28f0f3d47ff87fb3cdb9b2e9d
JTabel <juliantabel.jt@gmail.com>
2024-10-29 16:54:44 +0100

    full game

--- main.py
+++ main.py
@@ -3,19 +3,54 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    # objects
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
+    Shot.containers = (shots, drawable, updatable)
+
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        for u in updatable:
+            u.update(dt)
+        for a in asteroids:
+            if a.check_collision(player):
+                print("Game Over!")
+                exit(0)
+            for s in shots:
+                if s.check_collision(a):
+                    s.kill()
+                    a.split()
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
